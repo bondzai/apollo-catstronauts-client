c058a4ed860a0a5cb988a3e10d8036bb4bf7cada
JamesBond <introbond.dev@gmail.com>
2023-09-16 20:43:03 +0700

    Part III: Init

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -1,6 +1,7 @@
 const { RESTDataSource } = require('@apollo/datasource-rest');
 
 class TrackAPI extends RESTDataSource {
+  // the Catstronauts catalog is hosted on this server
   baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
 
   getTracksForHome() {
--- server/src/index.js
+++ server/src/index.js
@@ -7,24 +7,24 @@ const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
 async function startApolloServer() {
-    const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({ typeDefs, resolvers });
 
-    const { url } = await startStandaloneServer(server, {
-        context: async () => {
-            const { cache } = server;
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      const { cache } = server;
 
-            return {
-                dataSources: {
-                    trackAPI: new TrackAPI({ cache }),
-                },
-            };
+      return {
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
         },
-    });
+      };
+    },
+  });
 
-    console.log(`
-        🚀  Server is running
-        📭  Query at ${url}
-    `);
+  console.log(`
+    🚀  Server is running
+    📭  Query at ${url}
+  `);
 }
 
 startApolloServer();
